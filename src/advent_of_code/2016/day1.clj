550f402ad38ddad7a5709b556979b69e7a67c023
Oleh Sedletskyi <oleg.sedletskiy@gmail.com>
2024-11-17 18:25:54 +0000

    Add description

--- src/advent_of_code/2016/day1.clj
+++ src/advent_of_code/2016/day1.clj
@@ -1,6 +1,20 @@
 (ns advent-of-code.2016.day1
   (:require [clojure.string :as str]))
 
+;; The Document indicates that you should start at the given coordinates (where you just landed) and face North.
+;; Then, follow the provided sequence: either turn left (L) or right (R) 90 degrees, then walk forward the given
+;; number of blocks, ending at a new intersection.
+
+;; There's no time to follow such ridiculous instructions on foot, though, so you take a moment and work out the destination.
+;; Given that you can only walk on the street grid of the city, how far is the shortest path to the destination?
+
+;; For example:
+
+;; Following R2, L3 leaves you 2 blocks East and 3 blocks North, or 5 blocks away.
+;; R2, R2, R2 leaves you 2 blocks due South of your starting position, which is 2 blocks away.
+;; R5, L5, R5, R3 leaves you 12 blocks away.
+;; How many blocks away is Easter Bunny HQ?
+
 (def moving-map {:top {:right :right
                        :left :left}
                  :right {:right :bottom
